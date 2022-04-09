193fa031cac68c3311b72d0f596408b812e55407
Sergei (Mac) <sergeinosov6@gmail.com>
2022-04-09 07:45:39 +0300

    upd

--- FastSupportFixed/Controllers/MessagesController.cs
+++ FastSupportFixed/Controllers/MessagesController.cs
@@ -19,21 +19,21 @@ namespace FastSupportFixed.Controllers
         }
 
 
-        /*public Dictionary<int, string> GetLastMessages(string token)
-        {
-            return new Dictionary<int, string>()
-            {
-                { 1, "Hello!"},
-                { 1, "Where are my money!!!"},
-                { 0, "Stop! Wait please 5 minutes!"}
-            };
-        }*/
-
-
         [Route("[controller]/Message")]
         public IActionResult Message()
         {
-            return View();
+            return View(GetLastMessages("testToken"));
+        }
+
+
+        public Dictionary<string, int> GetLastMessages(string token)
+        {
+            return new Dictionary<string, int>()
+            {
+                {"Hello!", 1},
+                {"Where are my money!!!", 0},
+                {"Stop! Wait please 5 minutes!", 1}
+            };
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
