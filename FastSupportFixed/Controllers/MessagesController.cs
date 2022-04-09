6a409a9c6279a7d735df1089576aa8418a3dc431
Sergei (Mac) <sergeinosov6@gmail.com>
2022-04-09 09:52:34 +0300

    upd

--- FastSupportFixed/Controllers/MessagesController.cs
+++ FastSupportFixed/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,17 +7,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FastSupportFixed.Models;
+using MySqlConnector;
+using Microsoft.Extensions.Configuration;
+using System.Net;
 
 namespace FastSupportFixed.Controllers
 {
     public class MessagesController : Controller
     {
+        private IConfiguration _configuration;
         private readonly ILogger<MessagesController> _logger;
 
-        public MessagesController(ILogger<MessagesController> logger)
+        public MessagesController(ILogger<MessagesController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
+
 
 
         [Route("[controller]/Message")]
@@ -26,14 +33,45 @@ namespace FastSupportFixed.Controllers
         }
 
 
-        public Dictionary<string, int> GetLastMessages(string token)
+        public Dictionary<string, int> GetLastMessages(string userToken)
         {
             return new Dictionary<string, int>()
             {
                 {"Hello!", 1},
-                {"Where are my money!!!", 0},
-                {"Stop! Wait please 5 minutes!", 1}
+                {"Where are my money!!!", 1},
+                {"Stop! Wait please 5 minutes!", 0}
             };
+        }
+
+
+        [HttpGet, Route("[controller]/Send")]
+        public IActionResult SendMessage(string data)
+        {
+            
+            using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO `Chats` (`User`,`Messages`) VALUES ('freeToken','{data}')", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    /*
+                    while (reader.Read())
+                    {
+                    }*/
+
+                    
+                }
+            }
+
+            return new RedirectResult(url: "/Messages/Message", permanent: true,
+                            preserveMethod: true);
+        }
+
+        public ActionResult GetMessage()
+        {
+            string message = "Welcome";
+            return Ok();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FastSupportFixed/DataAnalysis/DataAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FastSupportFixed.DataAnalysis
+{
+    internal class DataAnalyzer
+    {
+        internal DataAnalyzer()
+        {
+            internal void GetKeyWords(string text)
+            {
+
+            }
+        }
+    }
+}
