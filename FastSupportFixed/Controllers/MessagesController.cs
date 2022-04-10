3520fa779a7bb06b282ecb88d7bf3dc90e31d39a
Sergei (Mac) <sergeinosov6@gmail.com>
2022-04-10 07:48:14 +0300

    upd

--- FastSupportFixed/Controllers/MessagesController.cs
+++ FastSupportFixed/Controllers/MessagesController.cs
@@ -10,6 +10,8 @@ using FastSupportFixed.Models;
 using MySqlConnector;
 using Microsoft.Extensions.Configuration;
 using System.Net;
+using FastSupportFixed.DataAnalysis;
+using Newtonsoft.Json;
 
 namespace FastSupportFixed.Controllers
 {
@@ -39,19 +41,54 @@ namespace FastSupportFixed.Controllers
             {
                 {"Hello!", 1},
                 {"Where are my money!!!", 1},
-                {"Stop! Wait please 5 minutes!", 0}
+                {"Stop! Wait please 5 minutes!", 0},
+                {"answer" , -1}
             };
         }
 
 
         [HttpGet, Route("[controller]/Send")]
-        public IActionResult SendMessage(string data)
+        public IActionResult SendMessage(string message)
         {
-            
+            //Костыль на проверку вопроса.
+
+            DataAnalyzer dataAnalyzer = new DataAnalyzer();
+
+            string messageCategory = "";
+
+            int emotionValue = dataAnalyzer.GetEmotionMessage(message);
+
+            string fastMessage = "";
+
+            if (emotionValue == 3 && !message.Contains("?"))
+            {
+                //Попросим пользователя болле конкретизировать своё обращение.
+                fastMessage = "Спасибо за обращение! Пожалуйста уточните детали в обращении";
+
+                return new RedirectResult(url: $"/Messages/Message?fm={fastMessage}", permanent: true,
+                          preserveMethod: true);
+            }
+
+            if (emotionValue == 0)
+            {
+                //Попросим пользователя болле конкретизировать своё обращение.
+                fastMessage = "Мы приняли вашу жалобу и рассмотрим её как можно скорее. Заранее приносим свои извинения.";
+                messageCategory = "report";
+            }
+
+            if(emotionValue == 1)
+            {
+                fastMessage = "Спасибо за обращение! Мы как можно скорее ответим на ваше обращение!";
+                messageCategory = "question";
+            }
+
+
+            KeywordsAnalizeInfo keywordsAnalizeInfo = dataAnalyzer.GetKeyWords(message);
+
             using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection")))
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand($"INSERT INTO `Chats` (`User`,`Messages`) VALUES ('freeToken','{data}')", conn);
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO `Chats` (`User`,`Messages`) VALUES ('freeToken','{message}')", conn);
 
                 using (var reader = cmd.ExecuteReader())
                 {
@@ -60,11 +97,33 @@ namespace FastSupportFixed.Controllers
                     {
                     }*/
 
-                    
+
+                    reader.Close();
+                    reader.Dispose();
                 }
+
+
+
+                cmd = new MySqlCommand($"INSERT INTO `UserRequests` (`Message`,`Keywords`, 'Category','SubCategory', 'ShortDescription') VALUES ('{message}','{JsonConvert.SerializeObject(keywordsAnalizeInfo.keywords)}','{messageCategory}','','','{keywordsAnalizeInfo.shortDescription}')", conn);
+
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    /*
+                    while (reader.Read())
+                    {
+                    }*/
+
+
+
+
+                    reader.Close();
+                    reader.Dispose();
+                }
+
             }
 
-            return new RedirectResult(url: "/Messages/Message", permanent: true,
+            return new RedirectResult(url: $"/Messages/Message?fm={fastMessage}", permanent: true,
                             preserveMethod: true);
         }
 
--- FastSupportFixed/DataAnalysis/DataAnalyzer.cs
+++ FastSupportFixed/DataAnalysis/DataAnalyzer.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using FastSupportFixed.DataModels;
+
 namespace FastSupportFixed.DataAnalysis
 {
     internal class DataAnalyzer
@@ -9,9 +12,47 @@ namespace FastSupportFixed.DataAnalysis
         }
 
 
-        internal void GetKeyWords(string text)
+        internal KeywordsAnalizeInfo GetKeyWords(string txt)
         {
-            
+            KeywordsAnalizeInfo keywordsAnalizeInfo = new KeywordsAnalizeInfo();
+
+            using (Pullenti.Ner.Processor proc = Pullenti.Ner.ProcessorService.CreateSpecificProcessor(Pullenti.Ner.Keyword.KeywordAnalyzer.ANALYZER_NAME))
+            {
+               
+
+                Pullenti.Ner.AnalysisResult ar = proc.Process(new Pullenti.Ner.SourceOfAnalysis(txt), null, null);
+              
+                foreach (Pullenti.Ner.Referent e in ar.Entities)
+                {
+                    //if (e is Pullenti.Ner.Keyword.KeywordReferent)
+                    //  Console.WriteLine(e);
+
+                    if (e is Pullenti.Ner.Keyword.KeywordReferent)
+                    {
+                        keywordsAnalizeInfo.keywords.Add(e.ToString());
+                    }
+
+                    if(e is Pullenti.Ner.Keyword.KeywordReferent b && b.Typ == Pullenti.Ner.Keyword.KeywordType.Annotation)
+                    {
+                        keywordsAnalizeInfo.shortDescription = e.ToString();
+                    }
+                }
+
+                /*
+                for (Pullenti.Ner.Token t = ar.FirstToken; t != null; t = t.Next)
+                {
+                    if (t is Pullenti.Ner.ReferentToken)
+                    {
+                        Pullenti.Ner.Keyword.KeywordReferent kw = t.GetReferent() as Pullenti.Ner.Keyword.KeywordReferent;
+                        if (kw == null)
+                            continue;
+                        string kwstr = Pullenti.Ner.Core.MiscHelper.GetTextValueOfMetaToken(t as Pullenti.Ner.ReferentToken, Pullenti.Ner.Core.GetTextAttr.FirstNounGroupToNominativeSingle | Pullenti.Ner.Core.GetTextAttr.KeepRegister);
+                        Console.WriteLine("{0} = {1}", kwstr, kw);
+                    }
+                }*/
+            }
+
+            return keywordsAnalizeInfo;
         }
 
         internal void GetShortDescription(string text)
@@ -39,5 +80,32 @@ namespace FastSupportFixed.DataAnalysis
             
         }
 
+
+        /// <summary>
+        /// 0 - positive
+        /// 1 - negative
+        /// 3 - neutral
+        /// </summary>
+        /// <returns></returns>
+        internal int GetEmotionMessage(string txt)
+        {
+            // Create single instance of sample data from first line of dataset for model input
+            SentimentModel.ModelInput sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = txt
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = SentimentModel.Predict(sampleData);
+            return Convert.ToInt32(predictionResult.PredictedLabel);
+        }
+
     }
+}
+
+
+internal class KeywordsAnalizeInfo
+{
+    public string shortDescription = string.Empty;
+    public List<string> keywords = new List<string>();
 }
